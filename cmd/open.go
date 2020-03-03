9b83b028809345ffd10eadfb41a968cc72d64fb0
Greg Answer <greganswer@gmail.com>
2020-03-02 23:31:02 -0500

    Finish open command

--- cmd/open.go
+++ cmd/open.go
@@ -3,6 +3,9 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/greganswer/mgit_go/git"
+	"github.com/greganswer/mgit_go/issues"
+	"github.com/pkg/browser"
 	"github.com/spf13/cobra"
 )
 
@@ -11,7 +14,16 @@ var openCmd = &cobra.Command{
 	Use:   "open",
 	Short: "Open an issue in the default web browser",
 	Run: func(cmd *cobra.Command, args []string) {
+		// Get issue from current branch.
+		currentBranch, err := git.CurrentBranch()
+		FailIfError(err)
+		issue, err := issues.FromBranch(currentBranch)
+		FailIfError(err)
+
+		// Open the issue in web browser.
 		fmt.Println("Opening issue...")
+		err = browser.OpenURL(issues.URL(issue.ID))
+		FailIfError(err)
 	},
 }
 
